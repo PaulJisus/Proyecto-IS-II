6e59fa89ecfd66152822e0bc0d87247def049302
PaulJisus <koddy_1234567890@hotmail.com>
2023-12-27 04:37:04 -0500

    final

--- src/models/asistente.py
+++
@@ -1,5 +0,0 @@
-from backend.models.usuario import UsuarioModel
-
-class AsistenteModel(UsuarioModel):
-    def __init__(self, id_, nombre_, apellido_, correo_):
-        UsuarioModel.__init__(self, id_, nombre_, apellido_, correo_)
--- src/models/ponente.py
+++
@@ -1,5 +0,0 @@
-from backend.models.usuario import UsuariosModel
-
-class PonenteModel(UsuariosModel):
-    def __init__(self, id, nombre, apellido, correo):
-        UsuariosModel.__init__(self, id, nombre, apellido, correo)
